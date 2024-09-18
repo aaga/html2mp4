5ad86445c9f47e939567db87a16ece18f8167a62
Ashwin Agarwal <me@ashwinagarwal.com>
2024-09-18 19:29:42 -0400

    Add basic audio

--- main.py
+++ main.py
@@ -3,21 +3,30 @@ from bs4 import BeautifulSoup
 from moviepy.editor import *
 from moviepy.video.tools.drawing import color_gradient
 
+import torch
+from TTS.api import TTS
+
+# Get device
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
 # MOVIE RESOLUTION
 w = 1280
 h = 720
 moviesize = w,h
 
+TEMP_AUDIO_FILE = "/tmp/tmp_audio.wav"
+
 class ShotList:
     def __init__(self, title = None):
-        if (title):
-            self.title = Title(title)
+        self.title = title
         self.scenes = []
     
-
-class Title:
-    def __init__(self, text):
-        self.text = text
+    def debug_print(self):
+        print(self.title)
+        for sc in self.scenes:
+            print("  ", sc.tag)
+            for sh in sc.shots:
+                print("    ", sh.tag, ": ", sh.get_plain_text(True))
 
 class Scene:
     def __init__(self, tag = None, name = None):
@@ -124,42 +133,68 @@ def main():
                 scene.shots.append(shot)
             shot_list.scenes.append(scene)
 
-        print(shot_list.title.text)
-        for sc in shot_list.scenes:
-            print("  ", sc.tag)
-            for sh in sc.shots:
-                print("    ", sh.tag, ": ", sh.get_plain_text(True))
+        shot_list.debug_print()
 
-        makeMovie(shot_list)
+        tts = TTS(model_name="tts_models/en/ljspeech/vits", progress_bar=True)
 
-def makeMovie(shot_list):
+        makeBasicMovie(shot_list, tts)
+
+def makeBasicMovie(shot_list, tts = None):
     clips = []
+    counter = 0
     for scene in shot_list.scenes:
         if (scene.tag == "h1"):
             composite = []
-            text = ""
+            paragraph_text = ""
+            speaking_text = ""
             for shot in scene.shots:
+                speaking_text += shot.get_plain_text() + "\n"
                 if (shot.tag == "h1"):
                     composite.append(TextClip(txt=shot.get_plain_text(True), color="black", method="caption", align="North", fontsize=72, font="Times-New-Roman-Bold", size=moviesize))
                 else:
-                    text += (shot.get_plain_text(True) + "\n\n")
-            if (text[-2:] == "\n\n"):
-                text = text[:-2] # Chop off last newlines
-            composite.append(TextClip(txt=text, color="black", method="caption", align="center", fontsize=36, font="Times-New-Roman", size=moviesize))
-            clips.append(CompositeVideoClip(composite, size=moviesize, bg_color=[255,255,255]).set_duration(5))
+                    paragraph_text += (shot.get_plain_text(True) + "\n\n")
+            if (paragraph_text[-2:] == "\n\n"):
+                paragraph_text = paragraph_text[:-2] # Chop off last newlines
+
+            composite.append(TextClip(txt=paragraph_text, color="black", method="caption", align="center", fontsize=36, font="Times-New-Roman", size=moviesize))
+            composite_clip = CompositeVideoClip(composite, size=moviesize, bg_color=[255,255,255])
+            if (tts):
+                counter += 1
+                filepath = "/tmp/" + str(counter) + ".wav"
+                tts.tts_to_file(speaking_text, file_path=filepath)
+                audio_clip = AudioFileClip(filepath)
+                composite_clip.audio = audio_clip
+                composite_clip = composite_clip.set_duration(audio_clip.duration)
+            else:
+                composite_clip = composite_clip.set_duration(5)
+            
+            clips.append(composite_clip)
         elif (scene.tag == "dl"):
             composite = []
+            speaking_text = ""
             for shot in scene.shots:
+                speaking_text += shot.get_plain_text() + "\n"
                 if (shot.tag == "dt"):
                     composite.append(TextClip(txt=shot.get_plain_text(True), color="black", method="caption", align="North", fontsize=36, font="Times-New-Roman-Bold", size=moviesize))
                 else:
                     composite.append(TextClip(txt=shot.get_plain_text(True), color="black", method="caption", align="center", fontsize=36, font="Times-New-Roman", size=moviesize))
-                    clips.append(CompositeVideoClip(composite, size=moviesize, bg_color=[255,255,255]).set_duration(5))
+                    composite_clip = CompositeVideoClip(composite, size=moviesize, bg_color=[255,255,255])
+                    if (tts):
+                        counter += 1
+                        filepath = "/tmp/" + str(counter) + ".wav"
+                        tts.tts_to_file(speaking_text, file_path=filepath)
+                        audio_clip = AudioFileClip(filepath)
+                        composite_clip.audio = audio_clip
+                        composite_clip = composite_clip.set_duration(audio_clip.duration)
+                    else:
+                        composite_clip = composite_clip.set_duration(5)
+                    clips.append(composite_clip)
                     composite = []
-
+                    speaking_text = ""
             
     final = concatenate_videoclips(clips)
-    final.write_videofile("out/test.mp4", fps=5)
+    filename = "out/" + shot_list.title + ".mp4"
+    final.write_videofile(filename, codec="libx264", temp_audiofile="/tmp/tmp_audio.m4a", remove_temp=True, audio_codec="aac", fps=5)
 
 
 if __name__ == "__main__":
